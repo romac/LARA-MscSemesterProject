dd6c551eef8f0c9dd8ef4e5af828925106cb0a7c
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-02-27 14:04:17 +0100

    Use different child encoding for Uniplate.

--- Samples/Uniplate.scala
+++ Samples/Uniplate.scala
@@ -14,92 +14,120 @@ object uniplate {
   final case class Val(value: Int)              extends Expr
   final case class Var(name: String)            extends Expr
 
-  // def children(a: Expr): List[Expr] =
-  //   uniplate(a)._1
+  sealed abstract class Childs {
+    def size: Int = this match {
+      case Zero()    => 0
+      case One(_)    => 1
+      case Two(_, _) => 2
+    }
 
-  // def universe(a: Expr): List[Expr] =
-  //   a :: children(a).flatMap(universe(_))
+    def toList: List[Expr] = this match {
+      case Zero()    => Nil()
+      case One(a)    => a :: Nil()
+      case Two(a, b) => a :: b :: Nil()
+    }
 
-  // def transform(f: Expr => Expr, a: Expr): Expr = {
-  //   val (childs, ctx) = uniplate(a)
-  //   f(ctx(childs.map(x => transform(f, x))))
-  // }
+    def map(f: Expr => Expr): Childs = this match {
+      case Zero()    => Zero()
+      case One(a)    => One(f(a))
+      case Two(a, b) => Two(f(a), f(b))
+    }
+  }
 
-  // def descend(f: Expr => Expr, a: Expr): Expr = {
-  //   val (childs, ctx) = uniplate(a)
-  //   ctx(childs.map(f))
-  // }
+  final case class Zero()                extends Childs
+  final case class One(a: Expr)          extends Childs
+  final case class Two(a: Expr, b: Expr) extends Childs
 
-  // def rewrite(f: Expr => Option[Expr], a: Expr): Expr = {
-  //   def g(x: Expr): Expr = f(x) match {
-  //     case None()   => x
-  //     case Some(x0) => rewrite(f, x0)
-  //   }
+  def children(a: Expr): List[Expr] =
+    uniplate(a)._1.toList
 
-  //   transform(g, a)
-  // }
+  def universe(a: Expr): List[Expr] =
+    a :: children(a).flatMap(universe(_))
 
-  // def nf(expr: Expr): Option[Expr] = expr match {
-  //   case Sub(x, y)           => Some(Add(x, Neg(y)))
-  //   case Add(x, y) if x == y => Some(Mul(x, Val(2)))
-  //   case _                   => None()
-  // }
+  def transform(f: Expr => Expr, a: Expr): Expr = {
+    val (childs, ctx) = uniplate(a)
+    f(ctx(childs.map(x => transform(f, x))))
+  }
 
-  // def rewriteNF(expr: Expr): Expr =
-  //   rewrite(nf, expr)
+  def descend(f: Expr => Expr, a: Expr): Expr = {
+    val (childs, ctx) = uniplate(a)
+    ctx(childs.map(f))
+  }
 
-  // val x = Sub(Neg(Var("q")), Var("q"))
+  def rewrite(f: Expr => Option[Expr], a: Expr): Expr = {
+    def g(x: Expr): Expr = f(x) match {
+      case None()   => x
+      case Some(x0) => rewrite(f, x0)
+    }
 
-  // def test = {
-  //   rewriteNF(x) == Mul(Var("q"), Val(2))
-  // } holds
+    transform(g, a)
+  }
 
-  def uniplate(expr: Expr): (List[Expr], List[Expr] => Expr) =
+  def nf(expr: Expr): Option[Expr] = expr match {
+    case Sub(x, y)           => Some(Add(x, Neg(y)))
+    case Add(x, y) if x == y => Some(Mul(x, Val(2)))
+    case _                   => None()
+  }
+
+  def rewriteNF(expr: Expr): Expr =
+    rewrite(nf, expr)
+
+  val x = Sub(Neg(Var("q")), Var("q"))
+
+  def test = {
+    rewriteNF(x) == Mul(Neg(Var("q")), Val(2))
+  } holds
+
+  def uniplate(expr: Expr): (Childs, Childs => Expr) =
     expr match {
 
       case Neg(a) => (
-        List(a),
-        (as: List[Expr]) => {
+        One(a),
+        (as: Childs) => {
           require(as.size == 1)
-          Neg(as.head)
+          val One(a0) = as
+          Neg(a0)
         }
       )
 
       case Add(a, b) => (
-        List(a, b),
-        (as: List[Expr]) => {
+        Two(a, b),
+        (as: Childs) => {
           require(as.size == 2)
-          Add(as.head, as.tail.head)
+          val Two(a0, b0) = as
+          Add(a0, b0)
         }
       )
 
       case Sub(a, b) => (
-        List(a, b),
-        (as: List[Expr]) => {
+        Two(a, b),
+        (as: Childs) => {
           require(as.size == 2)
-          Sub(as.head, as.tail.head)
+          val Two(a0, b0) = as
+          Sub(a0, b0)
         }
       )
 
       case Mul(a, b) => (
-        List(a, b),
-        (as: List[Expr]) => {
+        Two(a, b),
+        (as: Childs) => {
           require(as.size == 2)
-          Mul(as.head, as.tail.head)
+          val Two(a0, b0) = as
+          Mul(a0, b0)
         }
       )
 
       // case Div(a, b) => (
       //   List(a, b),
-      //   (as: List[Expr]) => {
+      //   (as: Childs) => {
       //     require(as.size == 2)
       //     Div(as.head, as.tail.head)
       //   }
       // )
 
       case x => (
-        Nil(),
-        (as: List[Expr]) => {
+        Zero(),
+        (as: Childs) => {
           require(as.size == 0)
           x
         }

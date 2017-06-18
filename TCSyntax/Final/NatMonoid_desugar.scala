415ac706ccc9c082f54b50df76287ac7f191dbc7
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-06-18 19:12:45 +0200

    Add Newtype testcase.

--- TCSyntax/Final/NatMonoid_desugar.scala
+++ TCSyntax/Final/NatMonoid_desugar.scala
@@ -3,26 +3,20 @@ import stainless.lang._
 import stainless.annotation._
 import stainless.proof._
 
-object Monoid_Nat_proof {
+object Monoid_Nat_desugar {
 
-  @coherent
-  abstract class Monoid[A] {
+  case class Monoid[A](empty: A, combine: (A, A) => A) {
 
-    def empty: A
+    require(law_leftIdentity && law_rightIdentity && law_associative)
 
-    def combine(x: A, y: A): A
-
-    @law
     def law_leftIdentity = forall { (x: A) =>
       combine(empty, x) == x
     }
 
-    @law
     def law_rightIdentity = forall { (x: A) =>
       combine(x, empty) == x
     }
 
-    @law
     def law_associative = forall { (x: A, y: A, z: A) =>
       combine(combine(x, y), z) == combine(x, combine(y, z))
     }
@@ -41,42 +35,32 @@ object Monoid_Nat_proof {
   final case class Succ(n: Nat) extends Nat
   final case class Zero()       extends Nat
 
-  // @inline
+  @inline
   @induct
   def lemma_leftIdentityZeroPlus(n: Nat): Boolean = {
     Zero() + n == n
   } holds
 
-  // @inline
+  @inline
   @induct
   def lemma_rightIdentityZeroPlus(n: Nat): Boolean = {
     n + Zero() == n
   } holds
 
-  // @inline
+  @inline
   @induct
   def lemma_associativePlus(n: Nat, m: Nat, l: Nat): Boolean = {
     (n + m) + l == n + (m + l)
   } holds
 
-  def natPlusMonoid: Monoid[Nat] = new Monoid[Nat] {
-
-    override def empty: Nat = Zero()
-
-    override def combine(a: Nat, b: Nat) = a + b
-
-    override def law_leftIdentity = forall { (x: Nat) =>
-      super.law_leftIdentity because lemma_leftIdentityZeroPlus(x)
+  def natPlusMonoid: Monoid[Nat] = {
+    require {
+      forall((x: Nat)                 => lemma_leftIdentityZeroPlus(x)) &&
+      forall((x: Nat)                 => lemma_rightIdentityZeroPlus(x)) &&
+      forall((x: Nat, y: Nat, z: Nat) => lemma_associativePlus(x, y, z))
     }
 
-    override def law_rightIdentity = forall { (x: Nat) =>
-      super.law_rightIdentity because lemma_rightIdentityZeroPlus(x)
-    }
-
-    override def law_associative = forall { (x: Nat, y: Nat, z: Nat) =>
-      super.law_associative because lemma_associativePlus(x, y, z)
-    }
-
+    Monoid[Nat](Zero(), _ + _)
   }
 
 }
---
+++ TCSyntax/Final/Newtype.scala
@@ -0,0 +1,102 @@
+
+import stainless.lang._
+import stainless.collection._
+import stainless.annotation._
+import stainless.proof._
+
+object NT {
+
+  @coherent
+  abstract class Newtype[N, O] {
+    def pack(o: O): N
+    def unpack(n: N): O
+
+    @law
+    def law_identity1 = forall { (o: O) =>
+      unpack(pack(o)) == o
+    }
+
+    @law
+    def law_identity2 = forall { (n: N) =>
+      pack(unpack(n)) == n
+    }
+
+  }
+
+  object Newtype {
+
+    def ala[N0, O0, N1, O1, B](pa: O0 => N0, hof: B => (O0 => N0) => N1)(b: B)
+      (implicit NT0: Newtype[N0, O0], NT1: Newtype[N1, O1]): O1 = {
+      NT1.unpack(hof(b)(NT0.pack _))
+    }
+
+  }
+
+}
+
+// object SumMonoid {
+
+//   @coherent
+//   abstract class Monoid[A] {
+
+//     def empty: A
+
+//     def append(x: A, y: A): A
+
+//     @law
+//     def law_leftIdentity = forall { (x: A) =>
+//       append(empty, x) == x
+//     }
+
+//     @law
+//     def law_rightIdentity = forall { (x: A) =>
+//       append(x, empty) == x
+//     }
+
+//     @law
+//     def law_associative = forall { (x: A, y: A, z: A) =>
+//       append(append(x, y), z) == append(x, append(y, z))
+//     }
+
+//   }
+
+//   final case class Sum(value: BigInt)
+
+//   implicit def sumMonoid = new Monoid[Sum] {
+//     def empty: Sum = Sum(0)
+//     def append(x: Sum, y: Sum): Sum = Sum(x.value + y.value)
+//   }
+
+//   def fold[A](list: List[A])(implicit M: Monoid[A]): A = list match {
+//     case Nil() => M.empty
+//     case Cons(x, xs) => M.append(x, fold(xs))
+//   }
+
+//   def foldMap[A, B](list: List[A])(f: A => B)(implicit M: Monoid[B]): B = {
+//     fold(list.map(f))
+//   }
+
+// }
+
+object test {
+
+  import NT._
+  import SumMonoid._
+
+  implicit def sumNewtype: Newtype[Sum, BigInt] = new Newtype[Sum, BigInt] {
+    def pack(o: BigInt): Sum = Sum(o)
+    def unpack(n: Sum): BigInt = n.value
+  }
+
+  def sum(list: List[BigInt]): BigInt = {
+    Newtype.ala((x: BigInt) => Sum(x), foldMap[BigInt, Sum] _)(list)
+  }
+
+  def lemma = {
+    val list: List[BigInt] = List(1, 2, 3, 4)
+    val res: BigInt = sum(list)
+    res == 10
+  } holds
+
+}
+
